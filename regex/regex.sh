d732db336edf5635d3148ba2c874db8b246ea83b
Elena <lam00071@morris.umn.edu>
2021-12-21 19:05:22 -0600

    attempting to use echo instead of old method to write to files

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 
-echo "$(sed -E 's/\*\s(\w+),\s(\w+)/1. \1\n2. \2\n/' < r0_input.txt)" > r0_output.txt
-echo "$(sed -E 's/\*\sI\sam\s([\w\s]+)\.\sMy\sfavorite\ssandwich\sis\s([\w\s]+)./1. \1\n2. \2\n/;t;d' < r1_input.txt)" > r1_output.txt
-echo "$(sed -E 's/\*\ssandwich\swith\s([\w.]{1,})\s([\w\s]+)/1. \1\n2. \2\n/' < r2_input.txt)" > r2_output.txt
+r0=$(sed -E 's/\*\s(\w+),\s(\w+)/1. \1\n2. \2\n/' < r0_input.txt)
+echo "$r0" > r0_output.txt
+
+r1=$(sed -E 's/\*\sI\sam\s([\w\s]+)\.\sMy\sfavorite\ssandwich\sis\s([\w\s]+)./1. \1\n2. \2\n/;t;d' < r1_input.txt)
+echo "$r1" > r1_output.txt
+
+r2=$(sed -E 's/\*\ssandwich\swith\s([\w.]{1,})\s([\w\s]+)/1. \1\n2. \2\n/' < r2_input.txt)
+echo "$r2" > r2_output.txt
