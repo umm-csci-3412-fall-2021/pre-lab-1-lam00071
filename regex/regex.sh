17790021ab182be4edc24e172ac8d06ec861ede3
Elena <lam00071@morris.umn.edu>
2021-12-21 19:12:22 -0600

    changed regex for files 2 and 1

--- regex/regex.sh
+++ regex/regex.sh
@@ -4,8 +4,8 @@
 r0=$(sed -E 's/\*\s(\w+),\s(\w+)/1. \1\n2. \2\n/' < r0_input.txt)
 echo "$r0" > r0_output.txt
 
-r1=$(sed -E 's/\*\sI\sam\s([\w\s]+)\.\sMy\sfavorite\ssandwich\sis\s([\w\s]+)./1. \1\n2. \2\n/;t;d' < r1_input.txt)
+r1=$(sed -E 's/\*\sI\sam\s([a-zA-Z\s]{1,})\.\sMy\sfavorite\ssandwich\sis\s([a-zA-Z\s]{1,})./1. \1\n2. \2\n/;t;d' < r1_input.txt)
 echo "$r1" > r1_output.txt
 
-r2=$(sed -E 's/\*\ssandwich\swith\s([\w.]{1,})\s([\w\s]+)/1. \1\n2. \2\n/' < r2_input.txt)
+r2=$(sed -E 's/\*\ssandwich\swith\s([a-zA-z.]+)\s([a-zA-z ]+)/1. \1\n2. \2\n/' < r2_input.txt)
 echo "$r2" > r2_output.txt
