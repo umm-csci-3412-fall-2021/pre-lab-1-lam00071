1a5df8f538200e429b89a746524889812c2cce9a
Elena <lam00071@morris.umn.edu>
2021-12-21 19:00:41 -0600

    trying alternative method for writing to files

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-file1=$(mkfile r0_output.txt)
-file2=$(mkfile r1_output.txt)
-file3=$(mkfile r2_output.txt)
 
-sed -E 's/\*\s(\w+),\s(\w+)/1. \1\n2. \2\n/' < r0_input.txt > $file1
-sed -E 's/\*\sI\sam\s([\w\s]+)\.\sMy\sfavorite\ssandwich\sis\s([\w\s]+)./1. \1\n2. \2\n/;t;d' < r1_input.txt > $file2
-sed -E 's/\*\ssandwich\swith\s([\w.]{1,})\s([\w\s]+)/1. \1\n2. \2\n/' < r2_input.txt > $file3
+echo "$(sed -E 's/\*\s(\w+),\s(\w+)/1. \1\n2. \2\n/' < r0_input.txt)" > r0_output.txt
+echo "$(sed -E 's/\*\sI\sam\s([\w\s]+)\.\sMy\sfavorite\ssandwich\sis\s([\w\s]+)./1. \1\n2. \2\n/;t;d' < r1_input.txt)" > r1_output.txt
+echo "$(sed -E 's/\*\ssandwich\swith\s([\w.]{1,})\s([\w\s]+)/1. \1\n2. \2\n/' < r2_input.txt)" > r2_output.txt
